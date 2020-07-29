1dbdd79b78228533a21b9127fa9dc48646ed6496
Bruno Lusa <bruno.lusa@gmail.com>
2020-07-29 19:02:32 -0300

    Refatorando healthCheckTest para utilizacao do Client-api-carros

--- src/main/java/basetest/BaseTest.java
+++ src/main/java/basetest/BaseTest.java
@@ -3,6 +3,7 @@ package basetest;
 import com.aventstack.extentreports.testng.listener.ExtentITestListenerAdapter;
 import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
 import com.lusa.carros.clients.DeleteCarrosClient;
+import com.lusa.carros.clients.GetActuatorClient;
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.BeforeSuite;
@@ -13,10 +14,12 @@ public class BaseTest {
 
     private static RequestSpecification requestSpecification;
     public static DeleteCarrosClient deleteCarrosClient;
+    public static GetActuatorClient getActuatorClient;
 
     @BeforeSuite
     public static void setUp() {
         deleteCarrosClient = new DeleteCarrosClient();
+        getActuatorClient = new GetActuatorClient();
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
     }
 }
--- src/test/java/healthcheck/HealthCheckTest.java
+++ src/test/java/healthcheck/HealthCheckTest.java
@@ -1,23 +1,18 @@
 package healthcheck;
 
+import basetest.BaseTest;
 import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.is;
 import static utils.PropertyLoader.retornarValorArquivoConfiguracao;
 
-public class HealthCheckTest {
+public class HealthCheckTest extends BaseTest {
 
     @Test
     public void validaSaudedaAplicacao(){
 
-        baseURI = retornarValorArquivoConfiguracao("baseUri");
-        port = Integer.parseInt(retornarValorArquivoConfiguracao("port"));
+        getActuatorClient.getHealthCheck();
 
-        when().
-            get("/actuator/health").
-        then().
-            statusCode(HttpStatus.SC_OK).
-            body("status", is("UP"));
     }
 }
