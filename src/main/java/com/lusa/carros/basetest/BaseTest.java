4be77f4f469cabe72ebe807c89350b50e3337f29
Bruno Lusa <bruno.lusa@gmail.com>
2020-07-29 23:05:38 -0300

    removendo dependencias nao utilizadas

--- src/main/java/com/lusa/carros/basetest/BaseTest.java
+++ src/main/java/com/lusa/carros/basetest/BaseTest.java
@@ -2,15 +2,12 @@ package com.lusa.carros.basetest;
 
 import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
 import com.lusa.carros.clients.*;
-import io.restassured.RestAssured;
-import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
 
 @Listeners({ExtentITestListenerClassAdapter.class})
 public class BaseTest {
 
-    private static RequestSpecification requestSpecification;
     public static DeleteCarrosClient deleteCarrosClient;
     public static GetActuatorClient getActuatorClient;
     public static GetCarrosClient getCarrosClient;
@@ -19,8 +16,6 @@ public class BaseTest {
 
     @BeforeSuite
     public static void setUp() {
-
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 
         deleteCarrosClient = new DeleteCarrosClient();
         getActuatorClient = new GetActuatorClient();
